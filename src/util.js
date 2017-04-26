c8f5a0347028f5d87bbfd1ae4c908073b1101e0f
Steffan <steffan@schiewe.biz>
2017-04-26 22:23:06 +0200

    add findexIndex()

--- src/util.js
+++ src/util.js
@@ -13,3 +13,41 @@ export function forEach(collection, callback) {
         callback.call(null, collection[key], key);
     });
 }
+
+/**
+ * Array.findIndex() polyfill.
+ */
+if (!Array.prototype.findIndex) {
+    Object.defineProperty(Array.prototype, 'findIndex', {
+
+        value(predicate) {
+
+            if (this == null) {
+                throw new TypeError('"this" is null or not defined');
+            }
+
+            if (typeof predicate !== 'function') {
+                throw new TypeError('predicate must be a function');
+            }
+
+            var o = Object(this);
+            var len = o.length >>> 0;
+            var thisArg = arguments[1];
+            var k = 0;
+
+            while (k < len) {
+
+                var kValue = o[k];
+
+                if (predicate.call(thisArg, kValue, k, o)) {
+                    return k;
+                }
+
+                k++;
+            }
+
+            return -1;
+        }
+
+    });
+}
