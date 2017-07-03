f8239c7cbe377a1b0a649de8bb0b5357298182e8
Steffan <steffan@schiewe.me>
2017-07-03 10:41:32 +0200

    export EventManager as default

--- src/index.js
+++ src/index.js
@@ -5,47 +5,46 @@
 import EventManager from './EventManager';
 import { forEach, isArray, isObject } from './util';
 
-function plugin(Vue) {
+var Events = new EventManager();
 
-    if (plugin.installed) {
+Events.install = function (Vue) {
+
+    if (this.installed) {
         return;
     }
 
-    var Events = new EventManager();
-    var version = Number(Vue.version.split('.')[0]);
+    Vue.events = this;
+    Vue.prototype.$events = this;
+    Vue.prototype.$trigger = this.trigger.bind(this);
+    Vue.mixin(Number(Vue.version[0]) < 2 ? {init: initEvents} : {beforeCreate: initEvents});
+};
 
-    function initEvents() {
+function initEvents() {
 
-        var {events} = this.$options, _events = [];
+    var {events} = this.$options, _events = [];
 
-        if (events) {
+    if (events) {
 
-            forEach(events, (listeners, event) => {
-                forEach(isArray(listeners) ? listeners : [listeners], listener => {
+        forEach(events, (listeners, event) => {
+            forEach(isArray(listeners) ? listeners : [listeners], listener => {
 
-                    var priority = 0;
+                var priority = 0;
 
-                    if (isObject(listener)) {
-                        priority = listener.priority;
-                        listener = listener.handler;
-                    }
+                if (isObject(listener)) {
+                    priority = listener.priority;
+                    listener = listener.handler;
+                }
 
-                    _events.push(Events.on(event, listener.bind(this), priority));
-                });
+                _events.push(Events.on(event, listener.bind(this), priority));
             });
+        });
 
-            this.$on('hook:beforeDestroy', () => _events.forEach(off => off()));
-        }
+        this.$on('hook:beforeDestroy', () => _events.forEach(off => off()));
     }
-
-    Vue.events = Events;
-    Vue.prototype.$events = Events;
-    Vue.prototype.$trigger = Events.trigger.bind(Events);
-    Vue.mixin(version < 2 ? {init: initEvents} : {beforeCreate: initEvents});
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
-    window.Vue.use(plugin);
+    window.Vue.use(Events);
 }
 
-export default plugin;
+export default Events;
